9bcbedbc51ff05f0ea754b99c834f281b0387b93
kingsley <kingsley2036@qq.com>
2021-04-23 00:16:36 +0800

    chore: ts初探

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObjet, useMount, useDebounce, useArray } from "utils/index";
+import qs from "qs";
+
+interface person {
+  name: string;
+  age: number;
+}
+const persons: person[] = [
+  {
+    name: "jack",
+    age: 25,
+  },
+  {
+    name: "ma",
+    age: 25,
+  },
+];
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const debounceValue = useDebounce(param, 1000);
+  const { value, add, removeIndex, clear } = useArray(persons);
+
+  const apiUrl = process.env.REACT_APP_API_RUL;
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObjet(param))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      }
+    );
+  }, [debounceValue, apiUrl]);
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel
+        param={param}
+        setParam={setParam}
+        users={users}
+      ></SearchPanel>
+      <List users={users} list={list}></List>
+    </div>
+  );
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,58 @@
+import { Interface } from "node:readline";
+import react, { useEffect, useState } from "react";
+const isFalsy = (value: any) => {
+  return value === 0 ? false : !value;
+};
+
+export const cleanObjet = (object: any) => {
+  const result = { ...object }; // 浅拷贝,只适用于简单对象
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: any) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value: any, delay: number) => {
+  const [debouneValue, setdebouneValue] = useState(value); // 初始状态是什么
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // value变化会导致一直重置定时器,所以setdebouneValue一直没有执行,直到value不变化
+      setdebouneValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+  return debouneValue; // 他其实一直在返回value,只是value一直变化时,debouneValue不变
+};
+interface arrayProp<T> {
+  name: string;
+  age: number;
+}
+export const useArray = <T>(array: T[]) => {
+  const [value, setValue] = useState(array);
+
+  return {
+    value,
+    add: (p: T) => {
+      setValue([...value, p]);
+    },
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+    clear: () => {
+      setValue([]);
+    },
+  };
+};
