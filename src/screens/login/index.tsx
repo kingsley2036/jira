011c0e5b617bcc53d79b6287ddcfa879643b89ed
kingsley2036 <kingsley2036@qq.com>
2021-04-23 17:29:52 +0800

    chore:  json-server 添加自定义api

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,39 @@
+import React, { FormEvent, FormEventHandler } from "react";
+const apiUrl = process.env.REACT_APP_API_RUL;
+export const Login = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const login = (param: { name: string; password: string }) => {
+      fetch(`${apiUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(param),
+      }).then(async (res) => {
+        if (res.ok) {
+          console.log("ok :>> ");
+        }
+      });
+    };
+    event.preventDefault();
+    const name = (event.currentTarget.elements[0] as HTMLFormElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLFormElement).value;
+    login({
+      name,
+      password,
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>用户名</label>
+        <input type="text" id="userName"></input>
+      </div>
+      <div>
+        <label>密码</label>
+        <input type="password" id="password"></input>
+      </div>
+      <button type="submit">登录</button>
+    </form>
+  );
+};
