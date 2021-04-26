d53e03527d9176fd399539af9bab442af899b862
kingsley <kingsley2036@qq.com>
2021-04-26 21:29:53 +0800

    chore: 保存登录状态

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,6 +1,8 @@
 import { createContext, useState, useContext, ReactNode } from "react";
 import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
+import { http } from "../utils/http";
+import { useMount } from "../utils";
 const AuthContext = createContext<
   | {
       user: User | null;
@@ -15,9 +17,22 @@ interface authForm {
   username: string;
   password: string;
 }
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http("me", { token });
+    user = data.user;
+  }
+  return user;
+};
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
+
   const login = (form: authForm) => {
     return auth.login(form).then((res) => {
       setUser(res);
