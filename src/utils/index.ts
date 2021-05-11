b51ecdc79d598045bdb9e3ae1a65986973ab60f4
kingsley <kingsley2036@qq.com>
2021-05-11 22:59:45 +0800

    fix: 去除警告信息

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+const isVoid = (value: unknown) =>
+  value === null || value === undefined || value === "";
 
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // @ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-      // @ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
