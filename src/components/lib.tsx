f10b873ad995c59fb6f564b42db458d3270cc817
kingsley <kingsley2036@qq.com>
2021-05-16 13:45:56 +0800

    feat: 处理me接口loading和error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,7 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
+import React from "react";
 
 export const Row = styled.div<{
   gap?: number | boolean;
@@ -21,3 +24,25 @@ export const Row = styled.div<{
         : undefined};
   }
 `;
+export const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const FullpageLoading = () => {
+  return (
+    <FullPage>
+      <Spin size={"large"} />
+    </FullPage>
+  );
+};
+export const FullPageError = ({ error }: { error: Error | null }) => {
+  return (
+    <FullPage>
+      <DevTools />
+
+      <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+    </FullPage>
+  );
+};
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,8 @@ import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
 import { http } from "../utils/http";
 import { useMount } from "../utils";
+import { useAsync } from "../utils/use-async";
+import { FullPageError, FullpageLoading } from "../components/lib";
 const AuthContext = createContext<
   | {
       user: User | null;
@@ -28,9 +30,19 @@ const bootstrapUser = async () => {
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  // const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    setData: setUser,
+    isIdle,
+    isLoading,
+    isError,
+    run,
+    error,
+  } = useAsync<null | User>();
   useMount(() => {
-    bootstrapUser().then(setUser);
+    // useEffect会默认在每次渲染之后时候执行
+    run(bootstrapUser());
   });
 
   const login = (form: authForm) => {
@@ -48,6 +60,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setUser(null);
     });
   };
+  if (isIdle || isLoading) {
+    return <FullpageLoading />;
+  }
+  if (isError) {
+    return <FullPageError error={error} />;
+  }
   return (
     <AuthContext.Provider
       children={children}
